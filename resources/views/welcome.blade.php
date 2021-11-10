8520d365a6ae359a7fe275cd181b58acf2a6d92a
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2021-11-10 02:06:00 +0200

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>laravel-google-sheet-app</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body class="antialiased">
+<section class="container border mt-2">
+    <p class="fw-bold fs-3 text text-decoration-underline">Upload your sheet from here</p>
+    <form>
+        <div class="input-group mb-3">
+            <label class="input-group-text" for="sheet">Sheet</label>
+            <input type="file" class="form-control" id="sheet">
+        </div>
+        <button type="submit" class="btn btn-primary mb-2">Upload</button>
+    </form>
+
+</section>
+<section class="container border mt-2 bg-dark">
+    <p class="fw-bold fs-3 text mt-1 text-center text-light">Or</p>
+</section>
+<section class="container border mt-2">
+    <p class="fw-bold fs-3 text text-decoration-underline">Sync from google sheet</p>
+    <form>
+        <button type="submit" class="btn btn-primary mb-2">Sync</button>
+    </form>
+</section>
+<section class="container border border-3 mt-5">
+    <p class="fw-bold fs-3 text text-decoration-underline">Contacts</p>
+    <table class="table  table-hover table-bordered border-secondary">
+        <thead class="bg-info text-light">
+        <tr>
+            <th>Name</th>
+            <th>Phone</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>ahmed</td>
+            <td>01221585306</td>
+        </tr>
+        </tbody>
+    </table>
+</section>
+</body>
+</html>
